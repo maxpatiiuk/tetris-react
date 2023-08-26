fccc155447433e2dae40edb478bde58238c9b5b6
Max Patiiuk <max@patii.uk>
2023-08-25 19:25:20 -0700

    refactor: Clean up block rendering

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -3,67 +3,13 @@ import Graphic from '@arcgis/core/Graphic';
 import SketchEdges3D from '@arcgis/core/symbols/edges/SketchEdges3D';
 import MeshSymbol3d from '@arcgis/core/symbols/MeshSymbol3d';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import FillSymbol3DLayer from '@arcgis/core/symbols/FillSymbol3DLayer';
 import MeshMaterial from '@arcgis/core/geometry/support/MeshMaterial';
+import type Point from '@arcgis/core/geometry/Point';
 import Mesh from '@arcgis/core/geometry/Mesh';
 import React from 'react';
 import { expose } from '../../utils/utils';
 import Color from '@arcgis/core/Color';
-import { type } from 'os';
-
-const size = 100;
-const color = (hue, opacity = 1) =>
-  Color.fromRgb(`hsla(${hue}, ${250}, ${250}, ${opacity})`);
-
-function display(view: SceneView) {
-  const clonePosition = view.camera.position.clone();
-  const offset = [0, 5 * size, -size];
-  clonePosition.x += offset[0];
-  clonePosition.y += offset[1];
-  clonePosition.z += offset[2];
-
-  const hue = Math.random() * 360;
-  const cubePolygon = Mesh.createBox(clonePosition, {
-    size: {
-      width: size / 2,
-      height: size / 2,
-      depth: size / 2,
-    },
-    material: new MeshMaterial({
-      color: color(undefined, 0.9),
-    }),
-  });
-
-  const fill = {
-    size: size * 0.75,
-    material: {
-      color: color(hue, 0.6),
-    },
-    edges: new SketchEdges3D({
-      size: size * 0.15,
-      color: color(hue, 1),
-      extensionLength: 2,
-    }),
-  };
-
-  // Create a Graphic for the extruded polygon
-  const cubeGraphic = new Graphic({
-    geometry: cubePolygon,
-    symbol: new MeshSymbol3d({
-      symbolLayers: [
-        {
-          type: 'fill',
-          ...fill,
-        },
-      ],
-    }),
-  });
-
-  const graphicsLayer = new GraphicsLayer();
-  graphicsLayer.add(cubeGraphic);
-  view.map.add(graphicsLayer);
-
-  expose({ cubeGraphic });
-}
 
 export function Game({ view }: { readonly view: SceneView }): null {
   React.useEffect(() => {
@@ -72,4 +18,57 @@ export function Game({ view }: { readonly view: SceneView }): null {
   }, []);
 
   return null;
+}
+
+const size = 100;
+const saturation = 250;
+const luminosity = 250;
+const color = (hue, opacity = 1) =>
+  Color.fromRgb(
+    `hsla(${Math.floor(hue)}, ${saturation}, ${luminosity}, ${opacity})`
+  );
+
+function display(view: SceneView) {
+  const clonePosition = view.camera.position.clone();
+  const offset = [0, 5 * size, -size];
+  clonePosition.x += offset[0];
+  clonePosition.y += offset[1];
+  clonePosition.z += offset[2];
+  const box = createBox(clonePosition);
+  const graphicsLayer = new GraphicsLayer();
+  graphicsLayer.add(box);
+  view.map.add(graphicsLayer);
+}
+
+function createBox(position: Point): Graphic {
+  const hue = Math.random() * 360;
+  const cubePolygon = Mesh.createBox(position, {
+    size: {
+      width: size / 2,
+      height: size / 2,
+      depth: size / 2,
+    },
+    material: new MeshMaterial({
+      color: color(hue, 0.9),
+    }),
+  });
+
+  // Create a Graphic for the extruded polygon
+  return new Graphic({
+    geometry: cubePolygon,
+    symbol: new MeshSymbol3d({
+      symbolLayers: [
+        new FillSymbol3DLayer({
+          material: {
+            color: color(hue, 0.6),
+          },
+          edges: new SketchEdges3D({
+            size: size * 0.15,
+            color: color(hue, 1),
+            extensionLength: 2,
+          }),
+        }),
+      ],
+    }),
+  });
 }
