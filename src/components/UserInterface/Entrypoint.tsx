2926115aa6b92c82d94cd399a2e6e459da18a549
Max Patiiuk <max@patii.uk>
2023-09-01 17:29:01 -0700

    feat: Complete basic layer selection

--- src/components/UserInterface/Entrypoint.tsx
+++ src/components/UserInterface/Entrypoint.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from '../MapRenderer/App';
+import { Tetris } from './';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Tetris />
   </React.StrictMode>,
 );
